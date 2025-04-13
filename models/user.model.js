d597356a2201d9dbfa4c3a8bf6f7756cfa11a055
ShreyashkumarDube <86827868+ShreyashkumarDube@users.noreply.github.com>
2025-04-13 19:41:47 +0530

    feat: update user model and authentication controller for improved user handling and error management

--- models/user.model.js
+++ models/user.model.js
@@ -1,31 +1,28 @@
 import mongoose from 'mongoose';
 
-
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'User name is required'],
-        trim: true,
-        minLength: [3, 'User name must be at least 3 characters long'],
-        maxLength: [50, 'User name must be at most 50 characters long'],
-    },
-    email : {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        match: [/^\S+@\S+\.\S+$/, 'Email is not valid'],
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minLength: [6, 'Password must be at least 6 characters long'],
-    },
-}, { timestamps: true }
-)
+  name: {
+    type: String,
+    required: [true, 'User Name is required'],
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'User Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'User Password is required'],
+    minLength: 6,
+  }
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
 export default User;
-
